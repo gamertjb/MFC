bbd56af1998c0ee65badfed173980c417e163aad
gamertjb <gamertjb@arcc2.uc.edu>
2025-07-16 15:42:27 -0400

    added sigma_2 to device list for simulation folder

---
+++ workflow_postp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+###############################################################################
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=64
+#SBATCH --ntasks=64
+#SBATCH -t 1-00:00:00
+#SBATCH -o mfc_test_%A.out
+#SBATCH -J mfc_test
+#SBATCH --exclusive
+
+###############################################################################
+## Modules
+module swap gnu9/9.4.0 gcc
+module load python fftw cmake
+module load openmpi/4.1.5
+module load anaconda/3.0
+conda activate MFC_env
+
+## Running MFC
+cd ~/MFC
+./mfc.sh run --debug /home/gamertjb/MFC/examples/2D_jet_SK/case.py -t post_process -b mpirun -n 64
